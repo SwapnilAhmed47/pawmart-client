5cdd19f920241837d0b10e67314200b1c268508d
Swapnil Ahmed <swapnilahmed2022@gmail.com>
2026-01-22 21:42:36 +0600

    installing firebase

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -21,9 +21,7 @@ export const router = createBrowserRouter([
                 element : <CategoryCard></CategoryCard>,
                 loader : ({params})=>fetch(`http://localhost:3000/products/${params.category}`)
             },
-            {
-                
-            }
+            
             
         ]
     }
